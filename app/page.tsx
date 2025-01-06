51d6723faa35c672b5697371ab2b1abfb0859804
ZHUANG JUNLING <108850976+JunlingZhuang@users.noreply.github.com>
2025-01-06 01:27:05 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is a financial dashboard to
+            <strong>Welcome to Cashboard.</strong> This is a financial dashboard to
             help manage your business invoices and revenue
           </p>
           <Link
